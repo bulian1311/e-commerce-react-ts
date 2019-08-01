231b64f6c434d8efd8a1ccbd3a69578f60952b6b
bulian1311 <bulian1311@gmail.com>
2019-08-01 18:29:24 +0300

    Add homePage component

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from '../homePage';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homePage/HomePage.tsx
@@ -0,0 +1,43 @@
+import React, { FC, ReactElement } from 'react';
+import './homePage.scss';
+
+const HomePage: FC = (): ReactElement => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNICKERS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MAN</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOOMAN</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
