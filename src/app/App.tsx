cf376d9203a614a4a769c03aef6ea32f2ac9cc34
bulian1311 <bulian1311@gmail.com>
2019-08-15 11:23:11 +0300

    lazy loading HomPage, ShopPage and CheckOutPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,17 @@
-import React, { FC, ReactElement, useEffect } from 'react';
+import React, { FC, ReactElement, useEffect, lazy, Suspense } from 'react';
 import './App.scss';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Header from '../components/header';
-import HomePage from '../pages/homePage';
-import { ShopPageContainer } from '../pages/shopPage';
-import CheckOutPage from '../pages/checkOutPage';
 import SignInAndSignUpPage from '../pages/signInAndSignUpPage';
 import { UserType, RootState } from '../utils/types';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { selectCurrentUser } from '../redux/user/userSelectors';
 import { checkUserSession } from '../redux/user/userActions';
+
+const HomePage = lazy(() => import('../pages/homePage'));
+const ShopPage = lazy(() => import('../pages/shopPage/ShopPageContainer'));
+const CheckOutPage = lazy(() => import('../pages/checkOutPage'));
 
 type PropsType = {
   currentUser: UserType,
@@ -27,14 +28,16 @@ const App: FC<PropsType> = ({ currentUser, checkUserSession }): ReactElement => 
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPageContainer} />
-        <Route
-          exact
-          path="/signin"
-          render={() => currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />}
-        />
-        <Route path="/checkout" component={CheckOutPage} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() => currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />}
+          />
+          <Route path="/checkout" component={CheckOutPage} />
+        </Suspense>
       </Switch>
     </div>
   );
